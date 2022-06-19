517d899e74b3862d841b4aef213c6231f05fd9a6
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2022-06-19 22:37:59 +0530

    WordPress config
    This files is used to change default config of wordpress

---
+++ wp-config-local.php
@@ -0,0 +1,15 @@
+<?php
+
+// ** URL settings ** //
+define( 'WP_ROOT_URL', 'http://localhost/demo' );
+define( 'WP_HOME', 'http://localhost/demo' );
+define( 'WP_SITEURL', 'http://localhost/demo/wp' );
+
+// ** MySQL settings - You can get this info from your web host ** //
+define( 'DB_NAME', 'demo' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', 'localhost' );
+
+// ** Never use cache while developing ** //
+define( 'WP_DEBUG', true );
---
+++ wp-salts.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service} You can change these at any
+ * point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+
+define('AUTH_KEY',         '.&ztUZ-PizM^u@32*BQUWL-o6C%r%6?[8(]C%lnw6%p<5w,ENp#@Z@WlbP0 t>wC');
+define('SECURE_AUTH_KEY',  '{^++%E/QNc$Na[7zQX(?6i-C]_Ffz<,mq05nN@Zi_/ 8-Y31amteW+8M[>xZb.%2');
+define('LOGGED_IN_KEY',    '|zBc9E`}/J_Py-S^@v}nbJ}N*7dO,ruYdvj[|AE|K&A%-ljNG1uXGstd[-#@-+I+');
+define('NONCE_KEY',        'i(p7dpd9Qy-G5sv`.ZZ|Yt|d:g.!Twyo|I+|)tSN7AUqF,#Q+nrKz7>0G:O{]8>Y');
+define('AUTH_SALT',        '}VELJSdx2Uqp>Bt>jw|]r#jg-dpW+~+i[a~)Ih|:H6r/Su78<?c4O_wu`dX|e qA');
+define('SECURE_AUTH_SALT', '3VpDI9)S%bANzidY]6c:}yDG2V 0 ^,t[>ro<szl${#bXG@{3j{*-|]j;KxF.$0G');
+define('LOGGED_IN_SALT',   'eyPWJms7|D+sm<|!Wb&|0RQ-tY#:mgmv6+e{Ic2P 4O&d!E!?{K55JZ|BLR<+-V9');
+define('NONCE_SALT',       'KV/bE`.Scf+Sre&IO-j/Ic-r1|*+S@wN]R1SBapihK4gey(>pcz2+[S)+7 y&(*M');
